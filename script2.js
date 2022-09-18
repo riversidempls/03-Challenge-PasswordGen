4c7662b4da7338ad7e08736727a6b18a0e849f2f
C. Donnelly <donn0021@umn.edu>
2022-09-18 15:02:25 -0500

    added JS input validation cause HTML wasn't doing it

--- script2.js
+++ script2.js
@@ -53,6 +53,12 @@
     }
     const passwordBox = document.getElementById("password");
     const length = document.getElementById("length");
+       //check length because html validation doesn't seem to be working
+       if ( length.value <= 8 || length.value > 128 ) {
+        alert("Please choose a length within range 8 - 128.");
+        return;
+      }
+      console.log(length.value);
     let password = "";
     while (length.value > password.length) {
       let paramToAdd = getParams[Math.floor(Math.random() * getParams.length)];
@@ -70,64 +76,3 @@
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", createPassword);
-
-
-
-
-
-
-//example
-
-// const keys = {
-//     upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-//     lowerCase: "abcdefghijklmnopqrstuvwxyz",
-//     number: "0123456789",
-//     symbol: "!@#$%^&*()_+~\`|}{[]:;?><,./-="
-//   }
-//   const getKey = [
-//     function upperCase() {
-//       return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
-//     },
-//     function lowerCase() {
-//       return keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
-//     },
-//     function number() {
-//       return keys.number[Math.floor(Math.random() * keys.number.length)];
-//     },
-//     function symbol() {
-//       return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
-//     }
-//   ];
-  
-//   function createPassword() {
-//     const upper = document.getElementById("upperCase").checked;
-//     const lower = document.getElementById("lowerCase").checked;
-//     const number = document.getElementById("number").checked;
-//     const symbol = document.getElementById("symbol").checked;
-//     if (upper + lower + number + symbol === 0) {
-//       alert("Please check atleast one box!");
-//       return;
-//     }
-//     const passwordBox = document.getElementById("passwordBox");
-//     const length = document.getElementById("length");
-//     let password = "";
-//     while (length.value > password.length) {
-//       let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
-//       let isChecked = document.getElementById(keyToAdd.name).checked;
-//       if (isChecked) {
-//         password += keyToAdd();
-//       }
-//     }
-//     passwordBox.innerHTML = password;
-//   }
-//   function copyPassword() {
-//     const textarea = document.createElement("textarea");
-//     const password = document.getElementById("passwordBox").innerText;
-//     if (!password) { return; }
-//     textarea.value = password;
-//     document.body.appendChild(textarea);
-//     textarea.select();
-//     document.execCommand("copy");
-//     textarea.remove();
-//     alert("Password copied to clipboard");
-//   }
